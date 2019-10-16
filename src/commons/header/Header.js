4446946c9f2f5e694a3c4bf461257befbb93fb77
AKILA K <akila.karthick89@gmail.com>
2019-10-16 09:47:57 +0530

    added header from material-ui

---
+++ src/commons/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+import Button from '@material-ui/core/Button';
+
+export default class Header extends Component {
+  render() {
+    return (
+        <div>
+          <Button variant="contained" color="default">LOGIN</Button>
+        </div>
+        )
+    
+  }
+}
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Header from "../../commons/header/Header";
+
+export default class Home extends Component {
+  render() {
+    return (
+        <div>
+            <Header />
+        </div>
+        )
+    
+  }
+}
