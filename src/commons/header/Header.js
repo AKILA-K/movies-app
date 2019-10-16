7c1ed01566e06f354ae8276bb1fcf88195a9aac2
AKILA K <akila.karthick89@gmail.com>
2019-10-16 17:58:02 +0530

    added TABS to login and register in  the login modal

--- src/commons/header/Header.js
+++ src/commons/header/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import logo from "../../assets/logo.svg";
 import Modal from "react-modal";
 
@@ -8,7 +10,8 @@ export default class Header extends Component {
     constructor(){
         super();
         this.state= {
-            modalIsOpen : false
+            modalIsOpen : false,
+            value : 0
         };
 
     }
@@ -33,8 +36,10 @@ export default class Header extends Component {
                     </div>
                 </header>
         <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
-
-
+        <Tabs value={this.state.value}>
+            <Tab label="LOGIN"></Tab>
+            <Tab label="REGISTER"></Tab>
+        </Tabs>
         </Modal>
         </div>
         )
