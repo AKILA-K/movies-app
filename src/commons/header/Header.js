42ca1b368f7b2b14c5b4c1f540a099184e703a9b
AKILA K <akila.karthick89@gmail.com>
2019-10-16 11:20:28 +0530

    added closemodalhandler to close the login modal

--- src/commons/header/Header.js
+++ src/commons/header/Header.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from "../../assets/logo.svg";
+import Modal from "react-modal";
 
 export default class Header extends Component {
+    constructor(){
+        super();
+        this.state= {
+            modalIsOpen : false
+        };
+
+    }
+
+    openModalHandler = () => {
+        this.setState({modalIsOpen : true})
+    }
+
+    closeModalHandler = () =>{
+        this.setState({modalIsOpen : false})
+    }
   render() {
     return (
         <div>
@@ -11,11 +27,15 @@ export default class Header extends Component {
           <header className="app-header">
               <img src={logo} className="app-logo" alt="logo" />
                     <div className="login-button">
-                        <Button variant="contained" color="default">
+                        <Button variant="contained" color="default" onClick={this.openModalHandler}>
                             Login
                         </Button>
                     </div>
                 </header>
+        <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+
+
+        </Modal>
         </div>
         )
     
