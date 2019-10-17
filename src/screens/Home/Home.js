0840319c2ac18ec53d37ff24b310cf7d551981bf
AKILA K <akila.karthick89@gmail.com>
2019-10-17 11:22:35 +0530

    added upcoming movies in the home page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import './Home.css';
 import Header from "../../commons/header/Header";
 
+const styles = theme => ({
+  root : {
+    flexgrow : 1,
+    backgroundColor : theme.palette.background.paper
+  },
+  upcomingMoviesHeading: {
+    textAlign : 'center',
+    background: '#ff9999',
+    padding :'8px',
+    fontSize : '1 rem' 
+  },
+  gridListUpcomingMovies : {
+    flexWrap : 'nowrap',
+    transform : 'translateZ(0)',
+    width : '100%'
+  }
+
+});
+
+
 export default class Home extends Component {
   render() {
     return (
         <div>
             <Header />
+            <div>
+              <span>Upcoming Movies</span>
+              </div>
         </div>
         )
     
